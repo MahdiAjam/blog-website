6eff53e35d66f5343379901cb33e4ff9668a21ad
MahdiAjam <mahdiajam8754@gmail.com>
2024-12-24 13:37:42 +0330

    added about.html and linking to home.html

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home/home.html')
 
-# Create your views here.
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'home/about.html')
